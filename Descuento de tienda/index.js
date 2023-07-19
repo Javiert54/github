dd4a220b2cef4b5f25587c7e6f95a03cf4439a07
Javiert54 <jdiazmach@gmail.com>
2023-07-19 16:32:23 +0100

    dfsf

--- Descuento de tienda/index.js
+++ Descuento de tienda/index.js
@@ -12,12 +12,12 @@
 // Escribir total
 // FinProceso
 function descuentos(){
-    var total=Number(document.getElementById("importe").value); //Hacemos que la variable "total" sea sobre la que escribimos directamente, para ahorrarnos una variable
-    var mes=document.getElementById("mes").value; //Leemos el mes
-    if(mes=="Octubre" || mes=="octubre"){ //Si el mes es octubre...
-        total*=0.85; //Aplicamos descuento
-    }
-    //Si no es ocutubre, no entramos en la cláusula IF, y no aplicamos descuento
+	var total=Number(document.getElementById("importe").value); //Hacemos que la variable "total" sea sobre la que escribimos directamente, para ahorrarnos una variable
+	var mes=document.getElementById("mes").value; //Leemos el mes
+	if(mes=="Octubre" || mes=="octubre"){ //Si el mes es octubre...
+		total*=0.85; //Aplicamos descuento
+	}
+	//Si no es ocutubre, no entramos en la cláusula IF, y no aplicamos descuento
 
-    alert("Dinero a pagar: "+total+"€");
+	alert("Dinero a pagar: "+total+"€");
 }
