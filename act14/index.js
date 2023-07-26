515dc974805dd4ddaed2014bf74bdd18d64d4aa5
Javiert54 <jdiazmach@gmail.com>
2023-07-26 18:24:19 +0100

    DYTYTRY

--- act14/index.js
+++ act14/index.js
@@ -1,2 +1,3 @@
 var diasSemana = new Array(7);
 diasSemana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sábado", "domingo"]
+document.write(diasSemana);
--- actividad15/act15.js
+++ actividad15/act15.js
@@ -1,3 +1,3 @@
 var nombres = [];
 nombres = ['Aitor Tilla', 'Elber Galarga', 'Paco Merte', 'Mónica Galindo', 'Benito Camela']
-for (let i = 0; i <= nombres.length; i++){document.write("</br>" + nombres[i]);}
+for (let i = 0; i < nombres.length; i++){document.write("</br>" + nombres[i]);}
