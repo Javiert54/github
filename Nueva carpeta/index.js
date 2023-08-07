71edcb6719d42b4cad7244147a38c8885b449f94
Javiert54 <jdiazmach@gmail.com>
2023-08-07 18:56:23 +0100

    fgh

---
+++ Nueva carpeta/index.js
@@ -0,0 +1,83 @@
+var links = JSON.parse(localStorage.getItem("links")) || [];
+
+$(function(){
+    rojo = $("#rojo");
+    rojo    
+        .css("background-color", "red")
+
+
+    amarillo = $("#amarillo");
+    amarillo
+        .css("background-color", "yellow")
+
+
+    $("#verde")
+        .css("background-color", "green")
+        
+    
+    
+    rojo.addClass("recuadro");
+    amarillo.removeClass("recuadro");
+    const recuadro = $(".recuadro");
+    recuadro
+        .css("border", "2px solid black")
+        .css("border-radius", "3%")
+        .css("font-size", "1.5em");
+    
+
+    $('[title="Google"]')
+        .css('color', "red");
+    
+    var contenedor = $("#contenedor");
+    // contenedor.mouseover(function(){
+    //     $(this).css("background-color", "red")
+    // })
+    // contenedor.mouseout(function(){
+    //     $(this).css("background-color", "green")
+    // })
+
+
+    function cambiaRojo(){
+        $(this).css("background-color", "red");
+    }
+    function cambiaVerde(){
+        $(this).css("background-color", "green");
+    }
+
+    contenedor.hover(cambiaRojo, cambiaVerde);
+
+    $(document).mousemove(function(){
+
+        $("html").css("cursor", "none");
+        $("#sigueme")
+            .css("left", event.clientX+1)
+            .css("top", event.clientY-1);
+
+    })
+
+    $("#add-button")
+        .click(function(){
+            $("#menu").prepend("<li><a href = '"+ $("#add-link").val() +"'>" + $("#add-link").val() + "</a></li>");
+            localStorage.setItem("links", JSON.stringify($("#add-link").val()));
+
+            $("#add-link").val("");
+
+        })
+    
+
+    $("#animar"/*document*/).mousemove(function(){
+        var caja = $("#contenedor");
+
+
+        caja.animate({
+            marginLeft: "400px",
+            fontSize: "40x",
+            height: "120px",//}, "slow")
+            // .animate({
+                borderRadious: "25%",
+                marginTop: "200px"
+            }, "fast")
+    })
+
+});
+
