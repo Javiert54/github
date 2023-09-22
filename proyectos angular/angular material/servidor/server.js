a46d75b494af38825e57ef0c1ecee1a885d2dec4
Javiert54 <jdiazmach@gmail.com>
2023-09-22 11:42:12 +0100

    update

--- proyectos angular/angular material/servidor/server.js
+++ proyectos angular/angular material/servidor/server.js
@@ -10,7 +10,7 @@ conectarDB()
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/usuario', require('./Routes/usuarioRoutes'))
+app.use('/api/users', require('./Routes/usuarioRoutes'))
 
 
 app.listen(3001, () => {
